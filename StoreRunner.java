a23e12aeca45a5f05573efe9e7d31e5d140881a4
LandonL1006 <llee0014@mymail.lausd.net>
2024-10-08 12:19:16 -0700

    improved code

--- StoreRunner.java
+++ StoreRunner.java
@@ -4,12 +4,28 @@ public class StoreRunner {
     public static void main(String[] args) {
 
         // Creating instances of VinylRecord and Instrument
-        VinylRecord v1 = new VinylRecord("Die Lit", "Playboi Carti", 19.99, 2016, "Rap");
+
+        System.out.println("Vinyl");
+
+        VinylRecord v1 = new VinylRecord("Die Lit", "Playboi Carti", 19.99, 2018, "Rap");
         System.out.println(v1);
 
+        VinylRecord v2 = new VinylRecord("Blonde", "Frank Ocean", 24.99, 2016, "R&B");
+        System.out.println(v2);
+      
+        VinylRecord v3 = new VinylRecord("This Is How Tomorrow Moves", "Beabadoobee", 28.99, 2024, "Rap");
+        System.out.println(v3);
+
+        System.out.println("Instruments");
+      
         Instrument i1 = new Instrument("Guitar", "Fender Stratocaster", 499.99, "Electric", "Wood");
         System.out.println(i1);
 
-        // You can add more instances as needed
+        Instrument i2 = new Instrument("Trumpet", "Etude ETR-100", 269.99, "Bass", "brass");
+        System.out.println(i2);
+
+        Instrument i3 = new Instrument("Piano", "Kawai CR-45 Crystal", 1500.00, "Grand", "Crystal");
+        System.out.println(i3);
+
     }
 }
